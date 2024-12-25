f24e61d7e2471d08e8158262b5821050bc781b53
João Pedro Diesel <jpedro.diesel@gmail.com>
2024-12-25 20:34:19 -0300

    Função de marcar tarefa como concluída feita

--- scripts.js
+++ scripts.js
@@ -1,12 +1,19 @@
-const newTask = document.getElementById('task-text');
+const taskInput = document.getElementById('task-text');
 const taskList = document.getElementById('task-list');
 const createNewTask = document.getElementById('create-task');
 
 function addNewTask() {
-  const taskListItem = document.createElement('li');
-  taskList.appendChild(taskListItem);
-  taskListItem.innerText = newTask.value;
-  newTask.value = '';
+  const newTask = document.createElement('li');
+  newTask.classList.add('task-item');
+  taskList.appendChild(newTask);
+  newTask.innerText = taskInput.value;
+  taskInput.value = '';
 }
 
+function selectTask(event) {
+  event.target.classList.toggle('selected');
+}
+
+
 createNewTask.addEventListener('click', addNewTask);
+taskList.addEventListener('click', selectTask);
